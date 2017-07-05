768687ec0c62181025e87a8a47de107211c20a0d
KCMac <kcmac@mac.local>
2017-07-05 15:05:50 +0800

    add ios udp demo

---
+++ ios/KCT_UDP_Demo/KCT_UDP_Demo/Config/AppHeaders.h
@@ -0,0 +1,30 @@
+//
+//  AppHeaders.h
+//  UdpPush
+//
+//  Created by KCMac on 2017/6/13.
+//  Copyright © 2017年 flypass. All rights reserved.
+//
+
+#ifndef AppHeaders_h
+#define AppHeaders_h
+
+
+//common
+#include "FlyRTCSDK.h"
+#import "VideoView.h"
+#include "ugo.h"
+
+//Vendor
+#import "MBProgressHUD+Add.h"
+
+//customView
+#import "LabelAndTextCell.h"
+#import "SegmentCell.h"
+#import "RadioCell.h"
+
+//viewContrller
+#import "PullViewController.h"
+#import "PushViewController.h"
+
+#endif /* AppHeaders_h */
